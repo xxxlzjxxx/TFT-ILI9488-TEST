add4e155b3e5750b95408f7dc43815d9922eb97d
xxxlzjxxx <1411795365@qq.com>
2018-07-19 17:06:37 +0800

    ili9488驱动中矩形填充通过画线修改好

--- SOFTWARE/F407_PACK_V11A-ILI9488/USER/main.c
+++ SOFTWARE/F407_PACK_V11A-ILI9488/USER/main.c
@@ -2,7 +2,9 @@
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
-#include "lcd.h"
+#include "ili9488.h"
+#include "key.h"
+#include "touch.h"
 
 //ALIENTEK 探索者STM32F407开发板 实验13
 //LCD显示实验-库函数版本
@@ -11,44 +13,38 @@
 //广州市星翼电子科技有限公司  
 //作者：正点原子 @ALIENTEK
 
+void GarminUI(void);
+
 int main(void)
 { 
- 	u8 x=0;
-	u8 lcd_id[12];				//存放LCD ID字符串
+// 	u8 x=0;
+//	u8 lcd_id[12];				//存放LCD ID字符串
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
 	delay_init(168);      //初始化延时函数
 	uart_init(115200);		//初始化串口波特率为115200
 	
 	LED_Init();					  //初始化LED
- 	LCD_Init();           //初始化LCD FSMC接口
-	POINT_COLOR=CL_RED;      //画笔颜色：红色
-	sprintf((char*)lcd_id,"LCD ID:%04X",lcddev.id);//将LCD ID打印到lcd_id数组。				 	
-  	while(1) 
-	{		 
-		switch(x)
-		{
-			case 0:LCD_Clear(CL_WHITE);break;
-			case 1:LCD_Clear(CL_BLACK);break;
-			case 2:LCD_Clear(CL_BLUE);break;
-			case 3:LCD_Clear(CL_RED);break;
-			case 4:LCD_Clear(CL_YELLOW);break;
-			case 5:LCD_Clear(CL_GREEN);break;
-			case 6:LCD_Clear(CYAN);break; 
-			case 7:LCD_Clear(CL_YELLOW);break;
-			case 8:LCD_Clear(BRRED);break;
-			case 9:LCD_Clear(GRAY);break;
-			case 10:LCD_Clear(LGRAY);break;
-			case 11:LCD_Clear(BROWN);break;
-		}
-		POINT_COLOR=CL_RED;	  
-		LCD_ShowString(30,40,210,24,24,"ILI9488 TEST");	
-		LCD_ShowString(30,70,200,16,16,"");
-		LCD_ShowString(30,90,200,16,16,"CUIT_BIKE");
- 		LCD_ShowString(30,110,200,16,16,lcd_id);		//显示LCD ID	      					 
-		LCD_ShowString(30,130,200,12,12,"2018/2/12");	      					 
-		x++;
-		if(x==5)x=0;
-		LED0=!LED0;	 
-		delay_ms(1000);	
-	} 
+ 	ILI9488_Init();           //初始化LCD FSMC接口
+	KEY_Init();				//初始化按键
+//	tp_dev.init();				//触摸屏初始化
+	while(1){
+		GarminUI();
+	}
+
 }
+
+
+void GarminUI(void){
+//先画好各个边界线
+	POINT_COLOR=BLUE;	
+	LCD_ShowxNum(0,40,10086,40,24,1);
+	LCD_Fill(0,88,320,98,BLACK);
+	LCD_DrawRectangle(0,186,320,196);
+	LCD_DrawRectangle(0,284,320,294);
+	LCD_DrawRectangle(0,382,320,392);
+	LCD_DrawRectangle(155,0,165,480);
+//显示信息
+}
+
+
+
